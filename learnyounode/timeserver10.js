a494e9051b86ed88627c36eae947c6424ffe9563
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2017-05-09 13:51:43 +0400

    Lesson 10 Completed
    Lesson 10 Time Server Completed

--- learnyounode/timeserver10.js
+++ learnyounode/timeserver10.js
@@ -1,9 +1,9 @@
 /**************************
  Node.JS
- 02.May.2017
+ 09.May.2017
 
  Core: LearnYouNode
- -- program10.js
+ -- timeserver10.js
 
 
   ## TIME SERVER (Exercise 10 of 13)
@@ -23,12 +23,45 @@
 
    After sending the string, close the connection.
 
- **************************/
+**************************/
+var net = require('net');
 
-var tsvr = require('./timeserver10.js');
 
 
-tsvr(process.argv[2]);
+module.exports = function(port) {
+
+    var time = function(){
+        var date = new Date();
+
+        var year =  date.getFullYear();
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+
+        if (month < 10) month = "0" + month;
+        if (day < 10) day = "0" + day;
+        if (hours < 10) hours = "0" + hours;
+        if (minutes < 10) minutes = "0" + minutes;
+
+        var tstr = year + "-" + month + "-" + day + " " + hours + ":" + minutes + "\n";
+
+        return tstr;
+    }
+
+    var server = net.createServer((socket) => {
+        socket.write(time());
+        socket.end();
+
+    });
+
+
+    server.listen(port);
+}
+
+
+
+
 
  /************************************************
  Official Solution:
