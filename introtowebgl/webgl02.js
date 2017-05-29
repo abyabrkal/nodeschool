534c139073ec876baf7ee4a33d4c02fb64fa2f6a
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2017-05-29 11:32:33 +0400

    Intro to WebGL started
    Intro to WebGL started 29 May 2017

---
+++ introtowebgl/webgl02.js
@@ -0,0 +1,14 @@
+/**
+ 
+ 02 Coordinates
+ Exercise 2 of 7
+
+
+ In the assets folder you will find 3 other folders:
+
+  * Boilerplate App - All three.js scenes require a bit of setup - a scene, camera, lighting & rendering loop. This app sets up a simple scene and is a good template to use for the exercises  
+  * Boilerplate physics - this setups a simple scene & the physics engine physijs (exercise 7)
+  * Goose model - a textured 3d model Goose (exercise 5)
+  * Game example - simple balancing game using physisjs physics engine
+ 
+*/
